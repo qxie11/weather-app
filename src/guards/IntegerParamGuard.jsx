acaf5fc0892e8c4cfe7648acb115df7076c1f0ec
Ponomarev Evgeniy <evgeniy.ponomarev@itechcraft.com>
2021-08-16 10:29:15 +0300

    Small changes

--- src/guards/IntegerParamGuard.jsx
+++ src/guards/IntegerParamGuard.jsx
@@ -3,11 +3,11 @@ import { Redirect, useParams } from 'react-router';
 const IntegerParamGuard = ({ children, paramName }) => {
     const param = useParams()[paramName];
 
-    if (!(/^\d{7}$/).test(param)) {
-        return <Redirect to="/search" />;
+    if ((/^\d{7}$/).test(param)) {
+        return children;
     }
 
-    return children;
+    return <Redirect to="/search" />;
 }
 
 export default IntegerParamGuard;
