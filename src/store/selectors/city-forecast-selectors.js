eb253d3fbe130b084e86fd324eaea13e93e3eff3
Ponomarev Evgeniy <evgeniy.ponomarev@itechcraft.com>
2021-08-16 10:32:37 +0300

    Refactor selectDividedHoursAndWeeklyForecast

--- src/store/selectors/city-forecast-selectors.js
+++ src/store/selectors/city-forecast-selectors.js
@@ -1,34 +1,33 @@
 import { createSelector } from '@reduxjs/toolkit';
-import moment from "moment";
+import moment from 'moment';
 import { last } from 'lodash';
 
-const selectCityForecastReducer = (state) => state.cityForecastReducer;
+const selectState = (state) => state.cityForecastReducer;
 
 export const selectCityForecast = createSelector(
-    selectCityForecastReducer,
+    selectState,
     (state) => state.cityForecast,
 );
 
-const filterForecast = (selector) => {
-    if (!selector) {
-        return {hourly: [], weekly: []};
-    }
-    const currentDay = moment().day();
-    return selector?.list?.reduce((acc, forecast, i) => {
-        if (moment(forecast.dt_txt).day() === currentDay) {
-            acc.hourly.push(forecast);
-        } else {
-            if (!acc.weekly.length || moment(last(last(acc.weekly)).dt_txt).day() !== moment(forecast.dt_txt).day()) {
-                acc.weekly.push([forecast]);
-            }
-
-            last(acc.weekly).push(forecast);
-        }
-        return acc;
-    }, {hourly: [], weekly: []});
-}
-
 export const selectDividedHoursAndWeeklyForecast = createSelector(
     selectCityForecast,
-    filterForecast
+    (cityForecast) => {
+        if (!cityForecast) {
+            return {hourly: [], weekly: []};
+        }
+        const currentDay = moment().day();
+        return cityForecast.list.reduce((acc, forecast) => {
+            if (moment(forecast.dt_txt).day() === currentDay) {
+                acc.hourly.push(forecast);
+            } else {
+                if (!acc.weekly.length || moment(last(last(acc.weekly)).dt_txt).day() !== moment(forecast.dt_txt).day()) {
+                    acc.weekly.push([forecast]);
+                    return acc;
+                }
+
+                last(acc.weekly).push(forecast);
+            }
+            return acc;
+        }, {hourly: [], weekly: []});
+    }
 );
